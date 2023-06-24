47dafc6f2537f3ee1937d83667cbacf626a97955
Saqlain Ahmed P <88509512+SAQLAINAP@users.noreply.github.com>
2023-06-24 22:38:48 +0530

    Add files via upload

---
+++ codes/quiz.py
@@ -0,0 +1,151 @@
+# name=input("enter your name")
+# print("hello",name)
+
+# score=0
+
+# #question 1
+# print("your first question is\n")
+# print("what is the capital of india")
+# print("a.delhi")
+# print("b.mumbai")
+# print("c.chennai")
+# print("d.bangalore")
+# options=['a','b','c','d']
+# answer=str(input("enter your choice"))
+# answer=answer.lower()
+# if str(answer)==options[0]:
+#     print("correct answer")
+#     score=score+10
+# else:
+#     print("incorrect answer")
+
+# print("your current score is",score,"\n")
+    
+
+# #question 2
+# print("Your second question is\n")
+# print("what is the currency of india")
+# print("a.dollar")
+# print("b.riyal")
+# print("c.Rupee")
+# print("d.Shelling")
+# options=['a','b','c','d']
+# answer=str(input("enter your choice"))
+# answer=answer.lower()
+# if str(answer)==options[2]:
+#     print("correct answer")
+#     score=score+10
+# else:
+#     print("incorrect answer")
+    
+# print("your current score is",score,"\n")    
+    
+# #question 3
+# print("Your third question is\n")
+# print("what is the national bird of india")
+# print("a.pigeon")
+# print("b.peacock")
+# print("c.falcon")
+# print("d.kingfisher")
+# options=['a','b','c','d']
+# answer=str(input("enter your choice"))
+# answer=answer.lower()
+# if str(answer)==options[1]:
+#     print("correct answer")
+#     score=score+10
+# else:
+#     print("incorrect answer")
+    
+# print("your current score is",score)    
+
+
+# #question 4
+# print("Your fourth question is\n")
+# print("who is the president of india")
+# print("a.ram nath kovind")
+# print("b.Amit shah")
+# print("c.rajnath singh")
+# print("d.Draupadi murmu")
+# options=['a','b','c','d']
+# answer=str(input("enter your choice"))
+# answer=answer.lower()
+# if str(answer)==options[3]:
+#     print("correct answer")
+#     score=score+10
+# else:
+#     print("incorrect answer")
+    
+# print("your current score is",score) 
+
+# #question 4
+# print("Your fifth question is\n")
+# print("which is indias national sport")
+# print("a.cricket")
+# print("b.hockey")
+# print("c.football")
+# print("d.volleyball")
+# options=['a','b','c','d']
+# answer=str(input("enter your choice"))
+# answer=answer.lower()
+# if str(answer)==options[1]:
+#     print("correct answer")
+#     score=score+10
+# else:
+#     print("incorrect answer")
+    
+# print("your current score is",score,"\n")
+
+# print("\n \n")
+
+
+# if score==50:
+#     print("youre a genius")
+# elif score>=40 and score<50:
+#     print("good job")
+# elif score>=30 and score<40:
+#     print("average")
+# elif score>=20 and score<30:
+#     print("can try better")
+# elif score>=10 and score<20:
+#     print("please improve for gods sake")
+# else:
+#     print("you are a waste person")
+    
+# print("welcome to the quiz\n")
+# points=0
+# print(f"current score is {points}")
+# q1={'question':'capital of india is', 'answer':'delhi'}
+# print(q1['question'])
+# ans=str(input("enter your answer \n"))
+# ans=ans.lower()
+# if ans== q1['answer']:
+#     print("correct")
+#     points=points+5
+# else:
+#     print("incorrect")
+# print(f"current score is {points}")
+    
+# q2={'question':'currency of india is', 'answer':'rupee'}
+# print(q2['question'])
+# ans=str(input("enter your answer \n"))
+# ans=ans.lower()
+# if ans== q2['answer']:
+#     print("correct")
+#     points=points+5
+# else:
+#     print("incorrect")
+# print(f"current score is {points}")
+
+# q3={'question':'pm of india is', 'answer':'modi'}
+# print(q3['question'])
+# ans=str(input("enter your answer \n"))
+# ans=ans.lower()
+# if ans== q3['answer']:
+#     print("correct")
+#     points=points+5
+# else:
+#     print("incorrect")
+# print(f"current score is {points}")
+
+
+    
